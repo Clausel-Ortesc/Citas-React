703dc6fa25b4eefa905b601ff00135c51777b01d
Claudia Ortega <ortegaescobedo4@gmail.com>
2022-10-25 02:34:36 -0500

    Creacion de los primeros 2 componentes
    dentro de la carpeta src, se crea una carpeta nueva llamada componentes en donde se crean 2 archivos de jsx llamadas header y header2 respectivamente, tambien se importan al archivo de App

---
+++ src/componentes/header.jsx
@@ -0,0 +1,6 @@
+function Header(){
+    return(
+        <h1>Administracion de citas</h1>
+    )
+}
+export default Header 
---
+++ src/componentes/header2.jsx
@@ -0,0 +1,6 @@
+function Header2(){
+    return(
+        <h2>Listado de Pacientes</h2>
+    )
+}
+export default Header2
